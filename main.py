64a0eed79095489a14854fcf3f340594a70d7276
Daniel <70908919+DanielVrublevskyi@users.noreply.github.com>
2025-10-04 13:50:38 +0300

    Homework 10.1

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-a = 5
-b = 10
+#a = 5
+#b = 10
 
-rectangle_area = a * b
-print("Area of rectangle = ", rectangle_area)
+#rectangle_area = a * b
+#print("Area of rectangle = ", rectangle_area)
+
+s = input("Enter snake_case text:")
+capital_camel_case = s.replace('_', " ").title().replace(' ', '')
+print("Capital camel case: ", capital_camel_case)
+print("lower camel case: ", capital_camel_case[0].lower() + capital_camel_case[1:])
